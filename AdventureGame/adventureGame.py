a5d492b7966813ec1dd2a9e48252c97821677c94
MarieMueller <m.mueller470718@gmail.com>
2022-10-27 21:41:31 -0500

    add file for project 2

---
+++ AdventureGame/adventureGame.py
@@ -0,0 +1,32 @@
+# Simple-version of an old-fashioned text-based adventure game
+# The game will present some scenarios and ask the user to make one of 2 choices, by entering 1 or 2
+
+# Step 1: Print descriptions of what's happening for the player
+
+# Step 2:Pausing between printing descriptions
+
+# Step 3: Give the player some choices
+
+# Step 4: Make sure the player gives a valid input
+
+# Step 5: Add functions and refactor your code
+
+# Step 6: Use randomness in your game
+
+# Step 7: Create win and lose conditions
+
+# Step 8: Check if the player wants to play again
+
+# Step 9: Check your style with pycodestyle
+
+# Step 10: Test Your Code
+
+"""
+Standout Suggestions
+Tips to personalize your project submissions and make them stand out:
+
+Identify the repetitive parts and refactor them into functions.
+Reduce redundancy by applying the DRY principle.
+Use intuitive function names and avoid generic names to provide clear context.
+Avoid using global variables in the functions.
+"""
